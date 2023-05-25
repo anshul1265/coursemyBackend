0501f22a77d22bda4343266c8b6312b49c737ef7
Anshul <Anshul@anshul.com>
2023-05-25 12:42:18 +0530

    fix the max. amount issue

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: "./config.env" });
 
+// connecting the database using mongoose
 export const connectDB = async () => {
   const { connection } = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB connected with ${connection.host}`);
